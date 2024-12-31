a144ac68f468227ceb1a9bf2b8e2609a327b883f
IldenH <IldenH.1@proton.me>
2024-12-31 20:24:59 +0100

    rust: simple-cli clap stuff

--- rust/simple-cli/src/main.rs
+++ rust/simple-cli/src/main.rs
@@ -1,3 +1,4 @@
+use clap::Command;
 use serde::Deserialize;
 use std::fs::read_to_string;
 
@@ -12,12 +13,29 @@ struct Stuff {
     yes: u32,
 }
 
+fn cli() -> Command {
+    Command::new("simple")
+        .about("A simple cli")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(Command::new("config").about("Shows info about config"))
+}
+
 fn main() {
+    let matches = cli().get_matches();
+
+    match matches.subcommand() {
+        Some(("config", _)) => print_config(),
+        _ => unreachable!(),
+    };
+}
+
+fn print_config() {
     let config_file = read_to_string("config.toml").unwrap();
     let config: Config = toml::from_str(&config_file).unwrap();
-    // dbg!(config);
 
     if config.is_yellow {
         println!("Yellow!")
     }
+    println!("yes: {}", config.stuff.yes)
 }
