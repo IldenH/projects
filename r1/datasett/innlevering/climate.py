756a87a0e40b2c7d0de05892d9b990d631b0575c
IldenH <IldenH.1@proton.me>
2025-04-03 10:06:15 +0200

    r1: clean up climate change

--- r1/datasett/innlevering/climate.py
+++ r1/datasett/innlevering/climate.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
 from scipy.optimize import curve_fit
 import numpy as np
 
-YEARS_IN_FUTURE = 30
+END_YEAR = 2025 + 30
 
 # https://www.ncei.noaa.gov/access/monitoring/climate-at-a-glance/global/time-series
 climate = pd.read_csv("climate-change.csv", delimiter=",")
@@ -18,13 +19,14 @@ def modell(t, a, b, c):
     return a * t**2 + b * t + c
 
 
-x = climate["Year"].dt.year.values.astype(int)
-y = climate["Anomaly"].values
+def make_modell(x, y):
+    koeffisienter, kovarians = curve_fit(modell, x, y)
+    x_modell = np.linspace(x[0], END_YEAR)
+    a, b, c = koeffisienter
+    y_modell = modell(x_modell, a, b, c)
 
-koeffisienter, kovarians = curve_fit(modell, x, y)
-x_modell = np.linspace(x[0], x[-1] + YEARS_IN_FUTURE)
-a, b, c = koeffisienter
-y_modell = modell(x_modell, a, b, c)
+    return (x_modell, y_modell)
+
 
 fig, ax1 = plt.subplots()
 
@@ -35,7 +37,13 @@ ax1.plot(
     label="Climate Change Anomaly Celsius",
 )
 ax1.tick_params(axis="y", labelcolor="red")
-ax1.set_ylabel("Grader celsius")
+ax1.set_ylabel("Klimaendringer avvik", color="red")
+ax1.yaxis.set_major_formatter(mticker.FuncFormatter(lambda val, _: f"{val:.1f}°C"))
+
+
+x = climate["Year"].dt.year.values.astype(int)
+y = climate["Anomaly"]
+x_modell, y_modell = make_modell(x, y)
 
 ax1.plot(
     pd.to_datetime(x_modell, format="%Y"),
@@ -44,22 +52,19 @@ ax1.plot(
     color="orange",
     linestyle="--",
 )
-ax1.scatter(pd.to_datetime(x, format="%Y"), y, label="Datapunkter", color="red")
+# ax1.scatter(pd.to_datetime(x, format="%Y"), y, label="Datapunkter", color="red")
 
 ax2 = ax1.twinx()
 ax2.plot(
     sea_level["Time"], sea_level["GMSL"], color="blue", label="Global Mean Sea Level"
 )
 ax2.tick_params(axis="y", labelcolor="blue")
-ax2.set_ylabel("Sjønivå millimeter")
+ax2.set_ylabel("Havnivå", color="blue")
+ax2.yaxis.set_major_formatter(mticker.FuncFormatter(lambda val, _: f"{val:.1f}mm"))
 
 x = sea_level["Time"].dt.year.values.astype(int)
-y = sea_level["GMSL"].values
-
-koeffisienter, kovarians = curve_fit(modell, x, y)
-x_modell = np.linspace(x[0], x[-1] + YEARS_IN_FUTURE)
-a, b, c = koeffisienter
-y_modell = modell(x_modell, a, b, c)
+y = sea_level["GMSL"]
+x_modell, y_modell = make_modell(x, y)
 
 ax2.plot(
     pd.to_datetime(x_modell, format="%Y"),
@@ -72,10 +77,7 @@ ax2.plot(
 
 plt.title("Klimaendringer og havnivå")
 ax1.set_xlabel("Årstall")
-
-plt.legend()
-ax1.legend()
-ax2.legend()
-# plt.tight_layout()
-plt.grid()
+fig.legend(loc="lower right")
+ax1.grid(color="pink")
+ax2.grid(color="lightblue")
 plt.show()
