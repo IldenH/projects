8c6d9a07a1bf7e27d10a6a7f463f90e2682182e2
IldenH <IldenH.1@proton.me>
2025-06-08 14:00:11 +0200

    rust: xmltest: parse timestamps

--- rust/xmltest/src/main.rs
+++ rust/xmltest/src/main.rs
@@ -1,12 +1,45 @@
 use quick_xml::de::from_str;
-use serde::Deserialize;
+use serde::{Deserialize, Deserializer};
 use serde_json::Value;
 use std::collections::HashMap;
 use std::fs::read_to_string;
+use time::OffsetDateTime;
+
+#[derive(Debug, Deserialize)]
+struct TimestampText {
+    #[serde(rename = "$text")]
+    text: String,
+}
+
+fn deserialize_xml_timestamp<'de, D>(deserializer: D) -> Result<Option<OffsetDateTime>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let timestamp_text_wrapper: Option<TimestampText> = Option::deserialize(deserializer)?;
+
+    match timestamp_text_wrapper {
+        Some(wrapper) => {
+            match OffsetDateTime::parse(
+                &wrapper.text,
+                &time::format_description::well_known::Rfc3339,
+            ) {
+                Ok(odt) => Ok(Some(odt)),
+                Err(e) => Err(serde::de::Error::custom(format!(
+                    "Failed to parse OffsetDateTime: {e}"
+                ))),
+            }
+        }
+        None => Ok(None),
+    }
+}
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 struct Call {
+    #[serde(default, deserialize_with = "deserialize_xml_timestamp")]
+    aimed_departure_time: Option<OffsetDateTime>,
+    #[serde(default, deserialize_with = "deserialize_xml_timestamp")]
+    actual_departure_time: Option<OffsetDateTime>,
     // aimed_arrival_time: Option<String>,
     // arrival_status: String,
     // destination_display: String,
@@ -14,15 +47,24 @@ struct Call {
     // aimed_departure_time: String,
     // expected_departure_time: String,
     // expected_arrival_time: String,
-    stop_point_name: Option<String>,
-    // stop_point_ref: String,
-    // #[serde(flatten)]
-    // extra: HashMap<String, Value>,
+    // stop_point_name: Option<String>,
+    stop_point_ref: String,
+    // cancellation: Option<bool>,
+    #[serde(flatten)]
+    extra: HashMap<String, Value>,
+}
+
+impl Call {
+    // fn delay(&self) -> {
+    //     self.actual_departure_time - self.aimed_departure_time
+    // }
 }
 
 #[derive(Debug, Deserialize)]
 struct JourneyCalls {
-    #[serde(rename = "EstimatedCall")]
+    // #[serde(rename = "EstimatedCall", alias = "RecordedCall")]
+    // #[serde(rename = "EstimatedCall")]
+    #[serde(rename = "RecordedCall")]
     calls: Vec<Call>,
 }
 
@@ -30,11 +72,18 @@ struct JourneyCalls {
 #[serde(rename_all = "PascalCase")]
 struct Journey {
     // #[serde(rename = "EstimatedCalls", alias = "RecordedCalls")]
-    #[serde(rename = "EstimatedCalls")]
+    // #[serde(rename = "EstimatedCalls")]
+    #[serde(rename = "RecordedCalls")]
     calls: Option<JourneyCalls>,
 
     #[serde(rename = "PublishedLineName")]
     line_name: Option<String>,
+    line_ref: String,
+    #[serde(rename = "OperatorRef")]
+    operator: Option<String>,
+    vehicle_mode: Option<String>,
+    #[serde(deserialize_with = "deserialize_xml_timestamp")]
+    recorded_at_time: Option<OffsetDateTime>,
 
     #[serde(flatten)]
     extra: HashMap<String, Value>,
@@ -76,9 +125,10 @@ fn main() {
             .map(|v| !v.calls.is_empty())
             .unwrap_or(false)
     }) {
-        println!("{}", journey.line_name.as_ref().unwrap());
-        for call in &journey.calls.as_ref().unwrap().calls {
-            println!("\t{}", call.stop_point_name.as_ref().unwrap());
-        }
+        // println!("{}", journey.line_name.as_ref().unwrap());
+        // for call in &journey.calls.as_ref().unwrap().calls {
+        //     println!("\t{}", call.stop_point_name.as_ref().unwrap());
+        // }
+        dbg!(journey);
     }
 }
