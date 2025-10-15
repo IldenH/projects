3c684524636e9b4f84d6cf843636e6d9d9935baf
IldenH <IldenH.1@proton.me>
2025-10-15 10:18:40 +0200

    fysikk: kule fluidmotstand

---
+++ fysikk1/kule_fluidmotstand.py
@@ -0,0 +1,19 @@
+from scipy import constants
+
+m = 0.0055
+g = constants.g
+k = 0.47
+s = 0
+v = 0
+t = 0
+dt = 0.001
+
+while s < 20/100:
+    G = m * g
+    L = k * v
+    a = (G - L) / m
+    v += a * dt
+    s += v * dt
+    t += dt
+
+print(t)
