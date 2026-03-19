810b9fb2706c9ca5d5c4f81973226b853a28a4e1
IldenH <IldenH.1@proton.me>
2026-03-19 10:03:39 +0100

    it1: search, display watched

--- it1/2026-vår-prosjekt/index.js
+++ it1/2026-vår-prosjekt/index.js
@@ -1,6 +1,41 @@
 const base_url = "http://127.0.0.1:8000";
 
 const recsEl = document.getElementById("recommendations");
+const searchEl = document.getElementById("search");
+const resultsEl = document.getElementById("results");
+const watchedEl = document.getElementById("watched");
+
+const watched = [
+  {
+    title: "Death Note",
+    rating: 3,
+  },
+  {
+    title: "Kimi no Na wa.",
+    rating: 10,
+  },
+];
+
+let animes = [];
+
+function renderResults(results) {
+  resultsEl.innerHTML = "";
+  results.forEach((item) => {
+    const li = document.createElement("button");
+    li.innerHTML = item;
+    resultsEl.appendChild(li);
+  });
+}
+
+searchEl.addEventListener("input", () => {
+  const query = searchEl.value.toLowerCase().trim();
+  const filtered = animes.filter((item) => item.toLowerCase().includes(query));
+  if (query != "") {
+    renderResults(filtered.slice(0, 20));
+  } else {
+    renderResults("");
+  }
+});
 
 async function getData(endpoint) {
   const response = await fetch(`${base_url}/${endpoint}`);
@@ -28,18 +63,32 @@ async function getRecommendations(watched) {
 }
 
 async function show() {
-  let watched = [
-    {
-      title: "Death Note",
-      rating: 9,
-    },
-  ];
+  watchedEl.innerHTML = watched
+    .map((w, _) => {
+      return `<article>${w.title} (${w.rating}/10)</article>`;
+    })
+    .join("");
+
   let recs = await getRecommendations(watched);
   console.log(recs);
   recs = recs["recommendations"].map((r, _) => {
     return `<li>${r.title}</li>`;
   });
   recsEl.innerHTML = `<ul>${recs.join("")}</ul>`;
+
+  await fetch("./export/anime_title_to_index.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Failed to load JSON");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      animes = Object.keys(data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
 
 show();
